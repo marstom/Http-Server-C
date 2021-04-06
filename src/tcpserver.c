53ff8d258de81ee018576cc356a47d41e22d47cb
Tomasz Marszałek <marstomek@gmail.com>
2021-04-06 13:48:55 +0200

    [hello]better naming

--- src/tcpserver.c
+++ src/tcpserver.c
@@ -39,20 +39,20 @@ int main(){
 }
 
 void handle_connection(int client_socket){
-    int n;
+    size_t bytes_read;
     uint8_t buff[MAXLINE+1];
     uint8_t recvline[MAXLINE+1];
     memset(recvline, 0, MAXLINE);
 
-    while( (n= read(client_socket, recvline, MAXLINE - 1))  > 0){
-        fprintf(stdout, "\n%s\n\n%s", bin2hex(recvline, n), recvline);
+    while( (bytes_read= read(client_socket, recvline, MAXLINE - 1))  > 0){
+        fprintf(stdout, "\n%s\n\n%s", bin2hex(recvline, bytes_read), recvline);
 
         //hacky way to detect end of request
-        if(recvline[n-1] == '\n')
+        if(recvline[bytes_read-1] == '\n')
             break;
         memset(recvline, 0, MAXLINE);
     }
-    if(n <0)
+    if(bytes_read <0)
         err_n_die("read error");
     
     // response
